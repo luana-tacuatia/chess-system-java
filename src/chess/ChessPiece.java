30e49c78ee4b6aa66455aed4460e3fabe339fb60
luana-tacuatia <tacuatia@yahoo.com.br>
2021-12-22 23:39:30 -0300

    feat: piece move count

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -7,10 +7,19 @@ import boardgame.Position;
 public abstract class ChessPiece extends Piece {
 
     private Color color;
+    private int moveCount;
 
     public ChessPiece(Board board, Color color) {
         super(board);
         this.color = color;
+    }
+
+    public void increaseMoveCount(){
+        moveCount++;
+    }
+
+    public void decreaseMoveCount(){
+        moveCount--;
     }
 
     public ChessPosition getChessPosition(){
@@ -19,6 +28,10 @@ public abstract class ChessPiece extends Piece {
 
     public Color getColor() {
         return color;
+    }
+
+    public int getMoveCount(){
+        return moveCount;
     }
     
     protected boolean isThereOpponentPiece (Position position){
